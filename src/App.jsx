150cf2c2deed4e4cb6a95dad2c6ff8a73f6790e2
viniciusrodmusic <aviniciusrodrigu@gmail.com>
2025-10-03 02:04:16 -0300

    chore: removi comentários e alterei title do index

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-// Importando css no jsx
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 import Input from './components/Input'
-// Onde coloca o sistema de rotas e layout padrão
+
 function App() {
 
   const [username, setUsername] = useState('viniciusrodmusic')
